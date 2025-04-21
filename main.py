895b070df7409bae2ee64e62dc3d695e86455633
User <dennismwema8@gmail.com>
2025-04-21 14:20:56 +0300

    updated the requirements file and added middlewares

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import openai
+import os
 
 
 app = FastAPI(title="AI Assistant Backend")
+
+# The Frontend Origin
+origins = ["http://localhost:3000"]
+
+# Middlewares
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 def root():
